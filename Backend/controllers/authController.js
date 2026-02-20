68cb9c2016a29950e0e77e34115e195b159db178
ayush patel <152726075+risingstar185@users.noreply.github.com>
2026-02-21 02:04:36 +0530

    Add path option to cookie in authController

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -18,6 +18,7 @@ export const GoogleAuth = async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: 'None',
+      path:"/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     }
     )
